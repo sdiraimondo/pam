b206d786d8cbc164874194c6d419e6b73336a335
Simon Long <simon@raspberrypi.org>
2017-01-23 16:56:26 +0000

    Add mechanism to insert warning message into terminal.

---
+++ etc/profile.d/sshpwd.sh
@@ -0,0 +1,6 @@
+if [ -e /home/pi/.sshwarn ] ; then
+    echo
+	echo "SSH is enabled and the default password for the 'pi' user has not been changed."
+	echo "This is a security risk - please login as the 'pi' user and type 'passwd' to set a new password."
+	echo
+fi
