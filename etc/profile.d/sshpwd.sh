7a4f6fa7a2a6da2fb9531e90ce8e93f87e2b595a
Simon Long <simon@raspberrypi.org>
2017-01-25 14:28:59 +0000

    Internationalisation support added for shell script.

--- etc/profile.d/sshpwd.sh
+++ etc/profile.d/sshpwd.sh
@@ -1,6 +1,10 @@
+export TEXTDOMAIN=Linux-PAM
+
+. gettext.sh
+
 if [ -e /var/lib/chksshpwd/sshwarn ] ; then
     echo
-	echo "SSH is enabled and the default password for the 'pi' user has not been changed."
-	echo "This is a security risk - please login as the 'pi' user and type 'passwd' to set a new password."
+	echo $(gettext "SSH is enabled and the default password for the 'pi' user has not been changed.")
+	echo $(gettext "This is a security risk - please login as the 'pi' user and type 'passwd' to set a new password.")
 	echo
 fi
