a95ec3d131c44bf4b152902cea79803ac9a4587e
Simon Long <simon@raspberrypi.org>
2017-01-25 17:02:53 +0000

    Tidying and additional error trapping added.

--- modules/pam_chksshpwd/pam_chksshpwd.c
+++ modules/pam_chksshpwd/pam_chksshpwd.c
@@ -3,15 +3,14 @@
 #include <crypt.h>
 
 /* Define which PAM interfaces we provide */
-  #define PAM_SM_SESSION
+#define PAM_SM_SESSION
 
-  /* Include PAM headers */
-  #include <security/pam_appl.h>
-  #include <security/pam_modules.h>
-  
+/* Include PAM headers */
+#include <security/pam_appl.h>
+#include <security/pam_modules.h>
   
 /* PAM entry point for session creation */
-int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **argv) 
+int pam_sm_open_session (pam_handle_t *pamh, int flags, int argc, const char **argv)
 {
 	char buf[1024], *salt;
 	FILE *fp;
@@ -28,27 +27,31 @@ int pam_sm_open_session(pam_handle_t *pamh, int flags, int argc, const char **ar
 	if (!pclose (fp)) return PAM_IGNORE;
 
 	// get the pi user line from the shadow file
-    if ((fp = popen ("grep -E ^pi: /etc/shadow", "r")) == NULL) return PAM_IGNORE;
-    fgets (buf, sizeof (buf) - 1, fp);
-    if (pclose (fp)) return PAM_IGNORE;
+	if ((fp = popen ("grep -E ^pi: /etc/shadow", "r")) == NULL) return PAM_IGNORE;
+	if (fgets (buf, sizeof (buf) - 1, fp) == NULL) return PAM_IGNORE;
+	if (pclose (fp)) return PAM_IGNORE;
 
-    // check for locked password, password disabled, strange ciphers etc - all indicate a change
-    if (!strncmp (buf, "pi:$", 4))
- 	{
+	// check for locked password or password disabled
+	if (!strncmp (buf, "pi:$", 4))
+	{
 		// password file entry as expected - check the password itself
 		salt = buf + 3;
-		strtok (salt, ":");
+		if (strtok (salt, ":") == NULL) return PAM_IGNORE;
+
+		// there is a properly-formatted entry in the shadow file - check the password
 		if (!strcmp (salt, crypt ("raspberry", salt)))
 		{
-		    fp = fopen ("/var/lib/chksshpwd/sshwarn", "wb");
-		    fclose (fp);
+			// password match - create the flag file
+			fp = fopen ("/var/lib/chksshpwd/sshwarn", "wb");
+			fclose (fp);
 		}
 	}
 
 	return PAM_IGNORE;
 }
 
-  /* PAM entry point for session cleanup */
-  int pam_sm_close_session(pam_handle_t *pamh, int flags, int argc, const char **argv) {
-          return PAM_IGNORE;
-  }
+/* PAM entry point for session cleanup */
+int pam_sm_close_session (pam_handle_t *pamh, int flags, int argc, const char **argv)
+{
+	return PAM_IGNORE;
+}
