dac93d9d8545606f3b08b7cfe4730805c6ee2c20
Simon Long <simon@raspberrypi.org>
2017-03-24 13:01:31 +0000

    Add gettext as a dependency for chksshpwd.

--- etc/profile.d/sshpwd.sh
+++ etc/profile.d/sshpwd.sh
@@ -4,7 +4,7 @@ export TEXTDOMAIN=Linux-PAM
 
 if [ -e /run/sshwarn ] ; then
     echo
-	echo $(gettext "SSH is enabled and the default password for the 'pi' user has not been changed.")
-	echo $(gettext "This is a security risk - please login as the 'pi' user and type 'passwd' to set a new password.")
+	echo $(/usr/bin/gettext "SSH is enabled and the default password for the 'pi' user has not been changed.")
+	echo $(/usr/bin/gettext "This is a security risk - please login as the 'pi' user and type 'passwd' to set a new password.")
 	echo
 fi
